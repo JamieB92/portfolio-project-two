4214eb08a6b6ae89ee0f3dbacfc8584064e10c7b
JamieB92 <j.bonner6992@gmail.com>
2023-03-25 23:58:38 +0000

    add content and finsih styl on index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,10 @@
 let startButton = document.querySelector("#start");
 
+
+
 let getPreviousScore = window.localStorage.getItem('Score');
 document.getElementById('previousScore').textContent = `Your previous score was ${getPreviousScore}/10`;
 
 startButton.addEventListener('click', function() {
-    localStorage.removeItem('Score');
+    localStorage.removeItem('key');
 })
